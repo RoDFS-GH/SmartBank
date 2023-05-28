336c8be6e49f6738428280dae93b49e1efbd7be1
RoDFS-GH <rosana.19abril@gmail.com>
2023-05-28 09:43:54 -0300

    publicando proyecto

---
+++ build/precache-manifest.bdddba4ff9a433d65eba5604ef663284.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d3ec26e953ccdc7c48351af6cf57a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48dbf3ad9f438b534e64",
+    "url": "/static/js/2.0ae3ce84.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.0ae3ce84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f0571e02af7f60f222",
+    "url": "/static/js/main.50cb90c9.chunk.js"
+  },
+  {
+    "revision": "927d5e5b2fc5e8a904b3",
+    "url": "/static/js/runtime-main.e6b65449.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/static/media/alimentacion.44dbaa78.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/static/media/dinero.b97c300b.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/static/media/logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/static/media/ojo.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/static/media/otros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/static/media/salud.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/static/media/utilidades.59035811.svg"
+  }
+]);
